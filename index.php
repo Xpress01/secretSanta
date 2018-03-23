a974f98ede628f05bc11d861d1f121c91f24762f
Vijay Ramdass <xpressenterprisellc@gmail.com>
2018-03-23 19:39:34 -0400

    Build V1

--- index.php
+++ index.php
@@ -5,14 +5,29 @@
 <meta charset="UTF-8">
 <title>ezGIFTex</title>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="/css/style.css">
+<link href="https://fonts.googleapis.com/css?family=Ramabhadra" rel="stylesheet">
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-3967759120531101",
+    enable_page_level_ads: true
+  });
+</script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-17991088-5"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-17991088-5');
+</script>
+
 </head>
 <body>
 
-<!-- Button trigger modal -->
-<button type="button" class="btn btn-primary newBTN" data-toggle="modal" data-target="#exampleModal">
-  New List
-</button>
+
 
 <!-- Modal -->
 <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" data-keyboard="false" data-backdrop="static">
@@ -32,32 +47,43 @@
     </div>
   </div>
 </div>
+
+<!-- Header -->
+<header id="header">
+  <img src="/img/logo.png" alt="logo" id="logo" class="img-responsive">
+  <div id="header-text">
+  <div id="registration-text">No Registration or Sign Up Required</div>
+  <button type="button" class="btn newBTN" id="new-btn" data-toggle="modal" data-target="#exampleModal">New Exchange</button>
+  </div>
+  
+</header>
     
 <!-- Form Body -->
  
 <div class="container">
+  <div class="form-bg">
     <table class="table table-striped table-bordered">
       <thead>
         <tr>
           <th scope="col">#</th>
           <th scope="col">Name</th>
-          <th scope="col">Email</th>
+          <th scope="col">Email Adress</th>
           <th scope="col">Exclude</th>
         </tr>
       </thead>
       <tbody class="form-container">
       </tbody>
     </table>
-</div>
-<div class="col">
     <button type="submit" class="btn btn-primary submitBTN">Submit</button>
+  </div>
 </div>
+
 
     
     
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
-<script src="app.js"></script>
+<script src="/js/app.js"></script>
 </body>
 </html>
